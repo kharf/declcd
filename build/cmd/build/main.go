b1847f173a134aac059b1f0b7c1ded8ce704d9e2
kharf <kevinfritz210@gmail.com>
2023-08-05 18:43:29 +0200

    build: add build and local testing with Dagger, remove Makefile and introduce github pr workflow via cue

---
+++ build/cmd/build/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kharf/declcd/build/internal/build"
+)
+
+func main() {
+	// Only build when tests pass
+	if err := build.RunWith(
+		build.Test,
+		build.Build,
+	); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ build/cmd/test/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kharf/declcd/build/internal/build"
+)
+
+func main() {
+	if err := build.RunWith(
+		build.Test,
+	); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
