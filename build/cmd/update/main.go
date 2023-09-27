ff5d6a78c0ca6889df1c2d678fb975c99f93a172
Kevin Fritz <44335182+kharf@users.noreply.github.com>
2023-09-27 07:41:37 +0200

    chore(deps): update renovate/renovate docker tag to v36.109 (#124)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- build/cmd/update/main.go
+++ build/cmd/update/main.go
@@ -28,7 +28,7 @@ func run() error {
 	pat := client.SetSecret("pat", os.Getenv("RENOVATE_TOKEN"))
 
 	updateContainer := client.Container().
-		From("renovate/renovate:36.107-full").
+		From("renovate/renovate:36.109-full").
 		WithDefaultArgs(dagger.ContainerWithDefaultArgsOpts{Args: []string{"kharf/declcd"}}).
 		WithEnvVariable("CACHEBUSTER", time.Now().String()).
 		WithEnvVariable("LOG_LEVEL", "DEBUG").
