fecf35f212d408bd2eac2ed957c934944803ffb1
kharf <kevinfritz210@gmail.com>
2024-05-10 15:44:38 +0200

    chore: add license header to code

--- pkg/helm/chart_test.go
+++ pkg/helm/chart_test.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package helm_test
 
 import (
@@ -206,7 +220,7 @@ func TestChartReconciler_Reconcile(t *testing.T) {
 					Values{},
 				)
 				return env, release, func(t *testing.T, env *kubetest.Environment, reconcileErr error, actualRelease *helm.Release, liveName, namespace string) {
-					assert.Error(t, reconcileErr, "auth secret value not found: host is empty")
+					assert.Error(t, reconcileErr, "Auth secret value not found: host is empty")
 				}
 			},
 			post: func(env projecttest.Environment, reconciler ChartReconciler, releaseDeclaration helm.ReleaseDeclaration) {
