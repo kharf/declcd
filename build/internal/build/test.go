373976bc74c9dc113149298e3fb0b11c5d247a9c
kharf <kevinfritz210@gmail.com>
2024-05-18 12:00:57 +0200

    test: update kubernetes version to v1.27 used for declcd testing

--- build/internal/build/test.go
+++ build/internal/build/test.go
@@ -44,7 +44,7 @@ func (t Test) run(ctx context.Context, request stepRequest) (*stepResult, error)
 		WithExec(
 			[]string{"go", "install", "sigs.k8s.io/controller-runtime/tools/setup-envtest@latest"},
 		).
-		WithExec([]string{envTest, "use", "1.26.1", "--bin-dir", localBin, "-p", "path"})
+		WithExec([]string{envTest, "use", "1.27", "--bin-dir", localBin, "-p", "path"})
 
 	apiServerPath, err := testBase.Stdout(ctx)
 	if err != nil {
