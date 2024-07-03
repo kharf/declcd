ddd90ee761a5869ba22720e95958bd6a61332f07
Kevin Fritz <44335182+kharf@users.noreply.github.com>
2024-07-03 07:06:30 +0200

    chore(deps): update golang docker tag to v1.22.5 (#528)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- build/internal/build/base.go
+++ build/internal/build/base.go
@@ -56,7 +56,7 @@ func RunWith(steps ...step) error {
 	defer client.Close()
 	goCache := client.CacheVolume("go")
 	base := client.Container().
-		From("golang:1.22.4-alpine").
+		From("golang:1.22.5-alpine").
 		WithExec([]string{"apk", "add", "--no-cache", "git"}).
 		WithExec([]string{"apk", "add", "--no-cache", "openssh-client"}).
 		WithExec([]string{"apk", "add", "--no-cache", "curl"}).
