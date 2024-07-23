a01439249cd7ab33d3d3ac964dd2aa24253ddeb2
Kevin Fritz <44335182+kharf@users.noreply.github.com>
2024-07-23 08:07:59 +0200

    chore(deps): update module github.com/goreleaser/goreleaser/v2 to v2.1.0 (#541)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- build/internal/build/build.go
+++ build/internal/build/build.go
@@ -71,7 +71,7 @@ func (p Publish) name() string {
 }
 
 // when changed, the renovate customManager has also to be updated.
-var goreleaserDep = "github.com/goreleaser/goreleaser/v2@v2.0.1"
+var goreleaserDep = "github.com/goreleaser/goreleaser/v2@v2.1.0"
 
 func (p Publish) run(ctx context.Context, request stepRequest) (*stepResult, error) {
 	reqVersion := p.Version
